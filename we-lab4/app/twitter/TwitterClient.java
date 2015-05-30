fadc5c3f285916e18edc251aaa82dc04003502df
Michael Brunner <michael.g.brunner@gmx.at>
2015-05-30 10:09:29 +0200

    enable multiple twitter postings

--- we-lab4/app/twitter/TwitterClient.java
+++ we-lab4/app/twitter/TwitterClient.java
@@ -2,24 +2,31 @@ package twitter;
 
 import twitter4j.*;
 import twitter4j.auth.AccessToken;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterClient implements ITwitterClient {
 
 
-    final String consumerKey = "GZ6tiy1XyB9W0P4xEJudQ";
-    final String consumerSecret = "gaJDlW0vf7en46JwHAOkZsTHvtAiZ3QUd2mD1x26J9w";
-    final String accessToken = "1366513208-MutXEbBMAVOwrbFmZtj1r4Ih2vcoHGHE2207002";
-    final String accessTokenSecret = "RMPWOePlus3xtURWRVnv1TgrjTyK7Zk33evp4KKyA";
+    final String CONSUMER_KEY = "GZ6tiy1XyB9W0P4xEJudQ";
+    final String CONSUMER_SECRET = "gaJDlW0vf7en46JwHAOkZsTHvtAiZ3QUd2mD1x26J9w";
+    final String ACCESSTOKEN = "1366513208-MutXEbBMAVOwrbFmZtj1r4Ih2vcoHGHE2207002";
+    final String ACCESSTOKEN_SECRET = "RMPWOePlus3xtURWRVnv1TgrjTyK7Zk33evp4KKyA";
 
+    @Override
     public void publishUuid(TwitterStatusMessage message) throws Exception {
-        Twitter twitter = TwitterFactory.getSingleton();
-        AccessToken token = new AccessToken(accessToken,accessTokenSecret);
-
-        twitter.setOAuthConsumer(consumerKey, consumerSecret);
-        twitter.setOAuthAccessToken(token);
-        twitter.getAuthorization();
-
+        AccessToken token = new AccessToken(ACCESSTOKEN, ACCESSTOKEN_SECRET);
         Status status = null;
+
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+        builder.setOAuthConsumerKey(CONSUMER_KEY);
+        builder.setOAuthConsumerSecret(CONSUMER_SECRET);
+        builder.setOAuthAccessToken(ACCESSTOKEN);
+        builder.setOAuthAccessTokenSecret(ACCESSTOKEN_SECRET);
+        Configuration configuration = builder.build();
+        TwitterFactory factory = new TwitterFactory(configuration);
+        Twitter twitter = factory.getInstance();
+
         try {
             status = twitter.updateStatus(message.getTwitterPublicationString());
         }catch(TwitterException e){
